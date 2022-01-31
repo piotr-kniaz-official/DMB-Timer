d827a0b0e3f397a042dc96d777f3b57bd4ec0e3f
Пётр <superpers2001@gmail.com>
2022-01-31 12:11:55 +0300

    Добавлено сообщение, если служба еще не началась.

--- Program.cs
+++ Program.cs
@@ -8,9 +8,10 @@ namespace DMB_Timer
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
+            var today = DateTime.Today;
             string buf;
 
-            Console.WriteLine("DMB Timer v.1.2\n");
+            Console.WriteLine("DMB Timer v.1.3\n");
             Console.WriteLine("Формат даты: DD.MM.YYYY\nНапример, 22.02.2022\n");
 
             Console.Write("Введите дату призыва: ");
@@ -18,6 +19,13 @@ namespace DMB_Timer
             if (!DateTime.TryParse(buf, out DateTime startDate))
             {
                 Console.WriteLine("Ошибка! Неверный формат даты.\nНажмите любую клавишу для выхода.");
+                Console.ReadKey(true);
+                return;
+            }
+            if (DateTime.Compare(startDate, today) > 0)
+            {
+                Console.WriteLine("\n\tСлужба еще не началась!");
+                Console.WriteLine("\n(C) Piotr Kniaz, 2022\nНажмите любую клавишу для выхода.");
                 Console.ReadKey(true);
                 return;
             }
@@ -38,7 +46,6 @@ namespace DMB_Timer
                 return;
             }
 
-            var today = DateTime.Today;
             var total = finishDate.Subtract(startDate);
             var left = finishDate.Subtract(today);
 
